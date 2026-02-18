3871f67aed33f64dc6729ea2cc7e1183d3421163
Rumon-20 <rumonpachani@gmail.com>
2026-02-18 20:03:22 +0530

    fixed errors

--- designverse/src/components/UserDashboard.tsx
+++ designverse/src/components/UserDashboard.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import connectDb from '@/lib/db'
-import ProductItemCard from './ProductItemCard'
 import { IProduct } from '@/models/product.model'
 import HeroSection from './HeroSection'
+import ProductItemCard from './ProductItemCard'
 
-async function UserDashboard({productList}:{productList:IProduct[]}) {
+async function UserDashboard({productList=[]}:{productList?:IProduct[]}) {
 await connectDb()
 const plainProduct = JSON.parse(JSON.stringify(productList))
 
   return (
     <>
       <HeroSection/>
-      <div className='w-[90%] md:w-[80%] mx-auto mt-10'>
+     <div className='w-[90%] md:w-[80%] mx-auto mt-10'>
         <h2 className='text-2xl md:text-3xl font-bold text-green-700 mb-6 text-center'>Popular Grocery Items</h2>
         <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6'>
           {plainProduct.map((item:any,index:number)=>(
@@ -20,7 +20,6 @@ const plainProduct = JSON.parse(JSON.stringify(productList))
       </div>
 
       </div>
-     
     </>
   )
 }
