74f6738384f10e7359359572c69a554f30f5efc3
Rumon-20 <rumonpachani@gmail.com>
2026-02-18 13:31:46 +0530

    herosection added

--- designverse/src/app/page.tsx
+++ designverse/src/app/page.tsx
@@ -1,10 +1,14 @@
 import { auth } from '@/auth'
+import AdminDashboard from '@/components/AdminDashboard'
+import DeliveryBoy from '@/components/DeliveryBoy'
 import EditRoleMobile from '@/components/EditRoleMobile'
 import Nav from '@/components/Nav'
+import UserDashboard from '@/components/UserDashboard'
 import connectDb from '@/lib/db'
 import User from '@/models/user.model'
 import { JSAnimation } from 'motion'
 import { redirect } from 'next/navigation'
+import { userAgent } from 'next/server'
 import React from 'react'
 
 async function Home() {
@@ -24,6 +28,11 @@ const plainUser=JSON.parse(JSON.stringify(user))
   return (
     <>
       <Nav user={plainUser}/>
+      {user.role=="user"?(
+        <UserDashboard/>
+      ):user.role=="admin"?(
+        <AdminDashboard/>
+      ): <DeliveryBoy/>}
     </>
   )
 }
